e56c2de8aa51b039c73c1bebe3496bfcdfdc0363
FuryAryan <yoyo26new@gmail.com>
2020-08-18 19:42:29 +0530

    made changes in in UI and added new paths

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404,render
 
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
+from django.urls import reverse
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -22,3 +24,32 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+   
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    def results(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
